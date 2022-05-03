7e0b90db1b0f96c9d6ea333b92ba7c11a4767611
Richard Jackson <jacksonr16@cardiff.ac.uk>
2022-05-03 13:58:36 +0100

    Part 3.5

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,41 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    q = Question.objects.order_by("pub_date")[:5]
+
+    context = {"latest_question_list": q}
+
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    context = {"q": q}
+    return render(request, "polls/detail.html", context)
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    print("HHHHH")
+    print(f"{request.POST}")
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "NOOOOOO"},
+        )
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
